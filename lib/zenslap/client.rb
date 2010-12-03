623a0e4a3ed21d8729b653c4ea6b506090d407d8
opsb <oliver@opsb.co.uk>
2010-12-03 09:27:32 +0000

    Can now specify zenslap host using environment variable '_ZENSLAP_CLIENT_HOST'

--- lib/zenslap/client.rb
+++ lib/zenslap/client.rb
@@ -1,10 +1,11 @@
 require 'rest-client'
 
 class ZenslapClient
+  ZENSLAP_HOST = ENV['_ZENSLAP_CLIENT_HOST'] || "zenslap.me"
   
   def self.configure(uuid, repo_owner, repo_name, github_credentials, heroku_app)
     #TODO need to make https
-    RestClient.post("http://zenslap.me/projects", 
+    RestClient.post("http://#{ZENSLAP_HOST}/projects", 
       {
         :project => github_credentials.merge({
                                               :owner => repo_owner, 
